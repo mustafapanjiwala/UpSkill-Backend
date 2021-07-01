45f7f5095d64b86ff761ef992c40c3dd8db5f85e
Sachin Motwani <sachinmotwani60@gmail.com>
2021-07-01 15:37:14 +0530

    dshgsdd

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose
     .then(() => console.log('Connected to MongoDB...'))
     .catch((err) => console.error('Could not connect to MongoDB...', err));
 
-app.use(express.json());
+app.use(express.json());   
 app.use('/api/users', users);
 app.use('/api/courses', courses);
 
